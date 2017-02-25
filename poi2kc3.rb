ca70f965dd41f08abecd915810eec3f868f382be
Hanna Reitz <xanclic@xanclic.moe>
2017-02-25 18:18:18 +0100

    Handle result packet

--- poi2kc3.rb
+++ poi2kc3.rb
@@ -55,13 +55,49 @@ out['support2'] = 0
 out['time'] = obj['time'] / 1000
 out['id'] = 1 # FIXME
 
-out['battles'] = [{
+day_battle = obj['packet'][0]
+if obj['packet'].length == 1
+    night_battle = {}
+    result_packet = {}
+elsif obj['packet'].length == 2
+    if obj['packet'][1]['api_win_rank']
+        night_battle = {}
+        result_packet = obj['packet'][1]
+    else
+        night_battle = obj['packet'][1]
+        result_packet = {}
+    end
+elsif obj['packet'].length == 3
+    night_battle = obj['packet'][1]
+    result_packet = obj['poacket'][2]
+else
+    die('Unexpected packet count')
+end
+
+assert(day_battle['api_hougeki1'] || day_battle['api_kouku'], 'Day battle packet is no day battle packet')
+assert(result_packet.empty? || result_packet['api_win_rank'], 'Result packet is no result packet')
+assert(night_battle.empty? || night_battle['api_hougeki'], 'Night battle packet is no battle packet')
+
+battle = {
     sortie_id: 1, # FIXME
     node: obj['map'][2],
     enemyId: 0, # FIXME
-    data: obj['packet'][0],
-    yasen: obj['packet'][1] ? obj['packet'][1] : {},
-    time: obj['packet'][0]['poi_time'] / 1000
-}]
+    data: day_battle,
+    yasen: night_battle,
+    time: day_battle['poi_time'] / 1000
+}
+
+if !result_packet.empty?
+    battle.merge({
+        rating: result_packet['api_win_rank'],
+        baseEXP: result_packet['api_get_base_exp'],
+        hqEXP: result_packet['api_get_exp'],
+        mvp: [result_packet['api_mvp']]
+    })
+
+    battle[:drop] = result_packet['api_get_ship']['api_ship_id'] if result_packet['api_get_ship']
+end
+
+out['battles'] = [battle]
 
 puts JSON.unparse(out)
